c65fddfb8591ca18f6cedb26c67d2532bb0c6a31
thecryptochampion <therealcryptochampion@gmail.com>
2026-04-18 07:44:00 -0700

    Remove Solflare to fix crypto error

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,7 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { Toaster } from "react-hot-toast";
 import { RPC_ENDPOINT } from "./utils/constants";
 import App from "./App";
@@ -19,10 +16,7 @@ require("@solana/wallet-adapter-react-ui/styles.css");
 
 const Root: FC = () => {
   const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
+    () => [new PhantomWalletAdapter()],
     []
   );
 
